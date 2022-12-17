6e0d4b73149b628fab16e1daba1b25b78917ed75
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-12-17 23:12:22 +0100

    Create graphColors.js

---
+++ src/utils/graphColors.js
@@ -0,0 +1,16 @@
+const graphColors = [
+  '#26b9de',
+  '#2fc17d',
+  '#5ec439',
+  '#9cdb43',
+  '#fae242',
+  '#ffc229',
+  '#f9a31b',
+  '#e35e24',
+  '#c72431',
+  '#d6246e',
+  '#e6229b',
+  '#bc2ce8',
+];
+
+export default graphColors;
